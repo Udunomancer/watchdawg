9e7df9ac21a2a0fa62fd47585d812b458180f528
Kyle Chriscoe <kyle717@gmail.com>
2021-02-23 15:27:54 -0500

    cleaning up default react app files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import {useEffect} from "react";
 import axios from "axios";
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   
@@ -16,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
