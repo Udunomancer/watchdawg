13b06cff6db50138b9465a8c6be11658632bbb30
Kyle Chriscoe <kyle717@gmail.com>
2021-02-23 17:57:22 -0500

    starting React configuration

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  
   useEffect(() => {
     axios
       .get("/api/config")
@@ -15,19 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
