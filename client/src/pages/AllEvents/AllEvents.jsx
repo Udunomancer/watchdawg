e24fdbf60b6f74efd630847983ba9354facff267
Kyle Chriscoe <kyle717@gmail.com>
2021-02-24 13:54:58 -0500

    basic All Events page

--- client/src/pages/AllEvents/AllEvents.jsx
+++ client/src/pages/AllEvents/AllEvents.jsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function AllEvents() {
-    return (
-        <div>
-            <h1>All Events will go here</h1>
-        </div>
-    );
-};
+  const [events, setEvents] = useState([
+    {
+      title: "",
+      description: "",
+      latitude: 0,
+      longitude: 0,
+      files: [],
+    },
+  ]);
+
+  useEffect(() => {
+    axios
+      .get("/api/events")
+      .then((response) => {
+        setEvents(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        {events.map((event) => (
+          <div className="col s4">
+            <div className="row">
+              <div className="col s12 m7">
+                <div className="card">
+                  <div className="card-image">
+                    <img
+                      alt="Sample"
+                      src="https://via.placeholder.com/200x100"
+                    />
+                    <span className="card-title">{event.title}</span>
+                  </div>
+                  <div className="card-content">
+                    <p>Description: {event.description}</p>
+                    <p>
+                      Location: {event.latitude}, {event.longitude}
+                    </p>
+                  </div>
+                  <div className="card-action">
+                    <a href="/">This is a link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default AllEvents;
