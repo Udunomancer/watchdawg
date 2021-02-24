143e5ab4d4e3c812f09b82c5062fc9f12e2353f7
Kyle Chriscoe <kyle717@gmail.com>
2021-02-24 13:18:10 -0500

    added Router Switch for React App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import { useEffect } from "react";
-import axios from "axios";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import AllEvents from "./pages/AllEvents/AllEvents";
+import EditEvents from "./pages/EditEvent/EditEvent";
+import Home from "./pages/Home/Home";
+import NewEvent from "./pages/NewEvent/NewEvent";
+import SingleEvent from "./pages/SingleEvent/SingleEvent";
+
 
 function App() {
-  
-  useEffect(() => {
-    axios
-      .get("/api/config")
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/events" component={AllEvents}/>
+          <Route exact path="/events/new" component={NewEvent}/>
+          <Route exact path="/events/:id/edit" component={EditEvents}/>
+          <Route exact path="/events/:id" component={SingleEvent}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
