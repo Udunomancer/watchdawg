cb3f6a247ce7a09a14f30408b53d3f82688911e2
Kyle Chriscoe <kyle717@gmail.com>
2021-03-01 15:48:48 -0500

    successfully getting image preview to appear

--- client/src/components/RecordForm/RecordForm.jsx
+++ client/src/components/RecordForm/RecordForm.jsx
@@ -1,42 +1,77 @@
 import React, { useState } from "react";
-import axios from "axios";
 
 function RecordForm() {
-    
-    const [record, setRecord] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [record, setRecord] = useState({
+    file: undefined,
+    preview: undefined,
+    progress: 0,
+    message: "",
+    imageDetails: {
+      title: "",
+      url: "",
+      description: "",
+      fileType: "",
+    },
+  });
 
-    function handleFileChange(e) {
-        setRecord(e.target.files[0]);
+  function selectFile(event) {
+    if (record.preview) {
+      URL.revokeObjectURL(record.preview);
     }
+    setRecord({
+      ...record,
+      file: event.target.files[0],
+      preview: URL.createObjectURL(event.target.files[0]),
+    });
+  }
 
-    async function handleFileSubmit() {
-        const formData = new FormData();
-        formData.append("file", record);
-        formData.append("upload_preset", process.env.CLOUDINARY_PRESET);
-        console.log(record);
-        console.log(formData);
-    }
-
-    return (
-        <>
-            <h1 className="center red-text">Image Upload</h1>
+  return (
+    <>
+      <div className="row">
+        <h1 className="center red-text">Record Upload</h1>
+      </div>
+      <div className="row">
+        <div className="col s6">
+          {record.preview ? (
+            <div className="preview">
+              <img
+                id="img-preview"
+                width="100%"
+                className="materialboxed"
+                src={record.preview}
+                alt="Upload Preview"
+              />
+            </div>
+          ) : (
             <div className="file-field input-field">
-                <div className="btn">
-                    <span>Browse</span>
-                    <input type="file" name="image" onChange={handleFileChange} />
-                </div>
-                <div className="file-path-wrapper">
-                    <input className="file-path validate" type="text" />
-                </div>
+              <div className="btn">
+                <span>Browse</span>
+                <input type="file" name="image" onChange={selectFile} />
+              </div>
+              <div className="file-path-wrapper">
+                <input className="file-path validate" type="text" />
+              </div>
             </div>
-            <div className="center">
-                <button onClick={handleFileSubmit} className="btn center">
-                    Upload
-                </button>
-            </div>
-        </>
-    )
+          )}
+        </div>
+        <div className="col s6">
+          <div className="input-field">
+            <input id="title" type="text"></input>
+            <label htmlFor="title">Record Title</label>
+          </div>
+          <div className="input-field">
+            <textarea id="description"></textarea>
+            <label htmlFor="description">Record Description</label>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="center">
+          <button className="btn center">Upload</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default RecordForm;
