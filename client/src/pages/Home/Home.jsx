39886021002b3cf3627cd7578cd4c977740b9e04
Kyle Chriscoe <kyle717@gmail.com>
2021-03-04 05:47:33 -0500

    added main page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,25 +2,27 @@ import React from "react";
 // --- import Material-UI components ---
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 // --- import customer Material UI styles ---
 import useStyles from "../../utils/Styles/Styles";
 
 function Home() {
-    
-    const classes = useStyles();
-    
-    return (
-        <main className={classes.content}>
-            <div className={classes.appBarSpacer} />
-            <Container maxWidth="lg" className={classes.container}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={8} lg={9}>
+  const classes = useStyles();
 
-                    </Grid>
-                </Grid>
-            </Container>
-        </main>
-    );
-};
+  return (
+    <main className={classes.content}>
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8} lg={9}>
+            <Typography component="h1" variant="h2" justify="center" align="center">
+              Welcome to WatchDawg!
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
+  );
+}
 
 export default Home;
